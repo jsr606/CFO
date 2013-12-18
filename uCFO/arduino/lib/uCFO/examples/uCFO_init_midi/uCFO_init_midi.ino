7f9782fcce998b9565d97973e9157dbea770d223
Jakob Bak <jakob.bak@gmail.com>
2013-12-18 15:29:19 +0100

    Updated example files and processing dashboard

--- uCFO/arduino/lib/uCFO/examples/uCFO_init_midi/uCFO_init_midi.ino
+++ uCFO/arduino/lib/uCFO/examples/uCFO_init_midi/uCFO_init_midi.ino
@@ -5,37 +5,20 @@
 #define MIDI_CHANNEL 1
 
 #include <SPI.h>
-#include <Friction.h>
-#include <MotorT3.h>
+#include <uCFO.h>
 
 void setup() {
 
   // We initialise the sound engine by calling Music.init() which outputs a tone
-  Serial.begin(115200);
-  MotorA.init();
-  MotorB.init();
   Music.init();
   Music.enableEnvelope1();
-  Music.enableEnvelope2();
-//  Midi.init();
+
   usbMIDI.setHandleNoteOff(OnNoteOff);
   usbMIDI.setHandleNoteOn(OnNoteOn);
-  usbMIDI.setHandleControlChange(OnControlChange);
-  
-  analogReadAveraging(32);
+  usbMIDI.setHandleControlChange(OnControlChange);  
 }
 
 void loop() {
 
   usbMIDI.read();
-  int valueA = analogRead(1);
-  int valueB = analogRead(8);
-//  Serial.print("valueA = ");
-//  Serial.print(valueA);
-//  Serial.print(" --- valueB = ");
-//  Serial.println(valueB);
-//  delay(50);
-  MotorA.torque((512-valueA)*4);
-  MotorB.torque((512-valueB)*4);
-  
 }
